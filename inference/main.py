9edfe20b25c449519fcc2db3137580cce3538af7
Stefan Guericke <stefan.guericke@gmail.com>
2024-02-08 20:52:42 +0100

    actual inference

--- inference/main.py
+++ inference/main.py
@@ -1,35 +1,69 @@
 from flask import Flask
 from flask_restx import Api, Resource
 
+import keras 
+from keras.utils import pad_sequences
+
 REST_API_PORT = 5000
+
+# perform preprocessing for inference
+
+# create word to index dictionary
+print("Creating word lookup")
+NUM_WORDS=50000 # only use top 50000 words
+MAX_WORDS = 200
+INDEX_FROM=3   # word index offset
+word_to_id = keras.datasets.imdb.get_word_index()
+word_to_id = {k:(v+INDEX_FROM) for k,v in word_to_id.items()}
+word_to_id["<PAD>"] = 0
+word_to_id["<START>"] = 1
+word_to_id["<UNK>"] = 2
+word_to_id["<UNUSED>"] = 3
+id_to_word = {value:key for key,value in word_to_id.items()}
+
+# deserialize keras model
+print("Deserializing Keras model")
+loaded_model = keras.saving.load_model("sentiment.keras")
+print("Done preprocessing")
 
 app = Flask(__name__)
 api = Api(app, 
           version='0.1', 
-          title='Gender Inference API', 
-          description='API that demos a simple inference mechanism by returning the gender of a name')
+          title='Sentiment Inference API', 
+          description='API that demos how to infer the sentiment out of a movie rating')
 
-ns = api.namespace('gender')
-@ns.route('/<string:firstname>')
+ns = api.namespace('sentiment')
+@ns.route('/<string:sentence>')
 @ns.response(200, 'Inference was successful')
-@ns.response(400, 'Firstname must contain only characters')
-@ns.param('firstname', 'The firstname whose gender should be returned')
+@ns.response(400, 'Invalid sentence provided')
+@ns.param('sentence', f'The sentence (max {MAX_WORDS} words) for which the sentiment is determined as "positive" or "negative"')
 class GenderInference(Resource):
-    def get(self,firstname):
+    def get(self,sentence):
         """
-            Inferes the gender based on a given firstname
+            Inferes the sentiment based on a sentence
         """
-        if not firstname or not firstname.isalpha():
-            api.abort(400, "Name should only contain characters")
+        if not sentence or len(sentence.split(" ")) > MAX_WORDS:
+            api.abort(400, "A sentence needs to be provided with a maximum of 200 words")
 
-        data = { "gender": "unknown" }
+        sentence_input = "<START> " + sentence.lower()
+        sentence_tokens = [word_to_id.get(i, word_to_id.get("<UNK>")) for i in sentence_input.split(" ")]
+        sentence_tokens
 
-        # here we perform the "prediction"
-        if firstname == "Stefan":
-            data['gender'] = "male"
-        elif firstname == "Nora":
-            data['gender'] = "female"
+        if None in sentence_tokens:
+            print("ERROR")
+
+        print(sentence_tokens)
+        inference_data = pad_sequences([sentence_tokens], maxlen=MAX_WORDS)
+        res = loaded_model.predict(inference_data)
+
+        if res:
+            data = {"sentiment" : "negative", "sentence_tokens" : sentence_tokens}
+            if res[0][0] > 0.5:
+                data["sentiment"] = "positive"
+        else:
+            api.abort(500, "something went terribly wrong")
+
         return data     
-        
+
 if __name__ == '__main__':
     app.run(port=REST_API_PORT)
---
+++ training/train.py
@@ -0,0 +1,64 @@
+from keras.datasets import imdb
+
+# use a subset of the data, load it and give an overview
+vocabulary_size = 50000
+
+# y=0: negative rating, y=1: positive rating
+(X_train_raw, y_train), (X_test_raw, y_test) = imdb.load_data(num_words = vocabulary_size)
+print('Loaded dataset with {} training samples, {} test samples'.format(len(X_train_raw), len(X_test_raw)))
+
+# get word data
+word2id = imdb.get_word_index()
+
+# sample of raw training data
+idx = 0
+review_len = 10
+print(f'IMDB review tokens: {X_train_raw[idx][:review_len]}..., label: {y_train[idx]}')
+
+# sample of underlying data
+id2word = {i: word for word, i in word2id.items()}
+review = [id2word.get(i, ' ') for i in X_train_raw[idx][:review_len]]
+print(f'IMDB review       : {review}..., label: {y_train[idx]}')
+
+# lets check the size of reviews
+print(f'Maximum review length: {len(max((X_train_raw + X_test_raw), key=len))}')
+print(f'Minimum review length: {len(min((X_train_raw + X_test_raw), key=len))}')
+
+from keras.utils import pad_sequences
+
+max_words = 200
+X_train = pad_sequences(X_train_raw, maxlen=max_words)
+X_test = pad_sequences(X_test_raw, maxlen=max_words)
+print(f'Maximum review length: {len(max((X_train + X_test), key=len))}')
+print(f'Minimum review length: {len(min((X_train + X_test), key=len))}')
+
+from keras.models import Sequential
+from keras.layers import Embedding
+from keras.layers import SimpleRNN,Dense,Activation,Dropout
+
+# simple RNN
+model = Sequential()
+
+model.add(Embedding(vocabulary_size,64,input_length =len(X_train[0])))
+model.add(SimpleRNN(32,input_shape = (vocabulary_size,max_words), return_sequences=False,activation="relu"))
+model.add(Dense(16,activation="relu"))
+model.add(Dropout(rate=0.5))
+model.add(Dense(1)) #flatten
+model.add(Activation("sigmoid")) # using sigmoid for binary classification
+
+print(model.summary())
+
+model.compile(loss='binary_crossentropy',
+             optimizer='adam',
+             metrics=['accuracy'])
+
+batch_size = 1000
+num_epochs = 5
+X_valid, y_valid = X_train[:batch_size], y_train[:batch_size]
+X_train2, y_train2 = X_train[batch_size:], y_train[batch_size:]
+history = model.fit(X_train2, y_train2, validation_data=(X_valid, y_valid), batch_size=batch_size, epochs=num_epochs)
+
+scores = model.evaluate(X_test, y_test, verbose=0)
+print('Test accuracy:', scores[1])
+
+model.save("sentiment.keras")
